b15fee203fef86351031f798f89879cd687e09a6
Iulian Bodnarescu <iulian.bodnarescu@trustyou.net>
2015-04-15 01:39:46 +0300

    first extractor almost over

--- extractors/winstanleysbikes.py
+++ extractors/winstanleysbikes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 from pyquery import PyQuery as pq
 from bike import bike
 import re
@@ -35,12 +36,42 @@ def extract_urls(req):
 
 def extract_data(req):
 	data = bike
+	domain = 'http://www.winstanleysbikes.co.uk'
 	w = pq(req["html"])
 
 	is_bike = w("table#prod_tbl.sectionborder_main img")
 	if is_bike:
-		name = w("table#item_Tbl.column_main tr:first").find("td").eq(1).text()
-		data["name"] = name
+		rows = w("table#item_Tbl.column_main tr")
+		for row in rows:
+			content = w(row)
+			if "Name" in content.children('td').eq(0).text():
+				data["name"] = content.children('td').eq(1).text()
+			if "Availability" in content.children('td').eq(0).text():
+				if "In stock" in content.children('td').eq(1).text():
+					data["availability"] = "A"
+				else:
+					data["availability"] = "O"
+			if "Size" in content.children('td').eq(0).text():
+				size = re.match(r"(\d+)(.+)", content.children('td').eq(1).text())
+				if size:
+					data["size_value"] = size.group(1)
+					if size.group(2) != "cm":
+						data["size_measure"] = "inch"
+			if "Product Code" in content.children('td').eq(0).text():
+				data["id"] = content.children('td').eq(1).text()
+			if "Price" in content.children('td').eq(0).text():
+				currency = re.match(r"(^[A-Z]{3}).*", content.children('td').eq(1).text())
+				if currency:
+					data["currency"] = currency.group(1)
+				price = re.match(r".*(\d{3,6}\.\d+),", content.children('td').eq(1).text())
+				if price:
+					data["price"] = price.group(1)
+					data["discounted_price"] = price.group(1)
+		data["provider_id"] = domain
+		image = w(".small_main>img").eq(0).attr("src")
+		if image:
+			data["image"] = domain + image
+		bike_description = 
 	#make that > 3
 	if len([item for item in data if data[item] != "N/A"]) >= 1:
 		return data
