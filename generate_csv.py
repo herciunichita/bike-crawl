609b7edb695849c53160c6e6f9688bac712efbd7
Stefan Marcian <stefan.marcian@trustyou.net>
2015-06-11 22:27:10 +0300

    updated generate csv

--- generate_csv.py
+++ generate_csv.py
@@ -16,6 +16,9 @@ if __name__ == '__main__':
 
 	csv_out = writer(stdout)
 
+	header = ["domain", "external_source_id", "url", "type", "name", "brand", "image","price", "discounted_price", "currency", "availability", "size_measure", "year", "frame", "gearset", "wheelset", "description"]
+	csv_out.writerow(header)
+
 	for line in stdin:
 		try:
 	
@@ -32,31 +35,16 @@ if __name__ == '__main__':
 			urls.add(data["url"])
 		else:
 			print >>stderr, data["url"]
-			continue
+			#continue
 		items.append(data)
 		if (data["external_source_id"], data["domain"]) not in id_domain:
 			items.append(data)
 			id_domain.add((data["external_source_id"], data["domain"]))
 		else:
 			print >>stderr, "Duplicate", (data["external_source_id"], data["domain"])
-			#continue
-		"""
-		if data["frame_material"] != "N/A" and data["frame"] == "N/A":
-			aux = data["frame_material"]
-			data["frame"] = aux
-			data["frame_material"] = "N/A"
-		"""
-		#del data["provider_id"]
-		#del data["size_value"]
-		data["gears"] = "N/A" if not data.get("gears") else data["gears"]
-		for item in data:
-			if data[item] != "N/A":
-				header.add(item)
-		#items.append(data)
-	header = list(header)
-	csv_out.writerow(header)
-	for item in items:
-		row = [item.get(key) for key in header]
+			continue	
+		
+		row = [data[item] for item in header]
 		csv_out.writerow(row)
 
 	
